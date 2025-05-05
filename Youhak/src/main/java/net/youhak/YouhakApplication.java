c3bbefe581e9b4e5f8fb4deb18ebbb7059e77268
dd-jiyun <dlwldbs8015@gmail.com>
2025-05-06 02:44:21 +0900

    infra: initial springboot settings

---
+++ Youhak/src/main/java/net/youhak/YouhakApplication.java
@@ -0,0 +1,13 @@
+package net.youhak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouhakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouhakApplication.class, args);
+	}
+
+}
---
+++ Youhak/src/test/java/net/youhak/YouhakApplicationTests.java
@@ -0,0 +1,13 @@
+package net.youhak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouhakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
