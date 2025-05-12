acadf5d24bd54f22a1be78299312e3ddc927920e
dd-jiyun <dlwldbs8015@gmail.com>
2025-05-12 21:29:40 +0900

    feat: add BaseEntity, Role, and RoleCategory domain models

---
+++ Youhak/src/main/java/net/youhak/member/role/domain/Role.java
@@ -0,0 +1,26 @@
+package net.youhak.member.role.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+import net.youhak.global.BaseEntity;
+
+@Entity
+@RequiredArgsConstructor
+public class Role extends BaseEntity {
+
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private RoleCategory roleCategory;
+
+    @Builder
+    public Role(String name, RoleCategory roleCategory) {
+        this.name = name;
+        this.roleCategory = roleCategory;
+    }
+}
