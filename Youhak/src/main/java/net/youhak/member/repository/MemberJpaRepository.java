450e4b30d0b0b5e8e376ae13ccae55841ccc1fb6
kbs1027 <rlaqjatn1027@gmail.com>
2025-05-21 02:00:12 +0900

    chore: change select member by email query

--- Youhak/src/main/java/net/youhak/member/repository/MemberJpaRepository.java
+++ Youhak/src/main/java/net/youhak/member/repository/MemberJpaRepository.java
@@ -3,8 +3,16 @@ package net.youhak.member.repository;
 import java.util.Optional;
 import net.youhak.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface MemberJpaRepository extends JpaRepository<Member, Long> {
+
+    @Query("""
+        select m
+        from Member m
+        where m.email = :email
+            and m.isDeleted = false
+    """)
     Optional<Member> findByEmail(@Param("email") String email);
 }
